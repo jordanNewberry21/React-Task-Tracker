32c1f6ce61e220c40b171a7ec28a591782b33ac2
Jordan Newberry <jordan.newberry@gmail.com>
2021-06-10 15:31:36 -0500

    delete route is working but not updating the dom

--- server/routes/task.router.js
+++ server/routes/task.router.js
@@ -10,7 +10,18 @@ router.get('/', (req, res) => { // get all tasks route
     }).catch((error) => {
       console.log('error retrieving inventory list from DB.....', error);
     });
-  });
+});
+
+router.delete('/:id', (req, res) => { // delete task by id
+    const id = req.params.id;
+    const sqlText = `DELETE FROM tasks WHERE id=$1;`;
+    pool.query(sqlText, [id]).then(result => {
+      console.log('task successfully deleted......')
+    }).catch(error => {
+      console.log('error deleting item from inventory........', error);
+      res.sendStatus(500);
+    });
+});
 
 
   module.exports = router;
