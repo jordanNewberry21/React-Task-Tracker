df3bf6bacf4e26350cac2a2ad76e6d6b99198fee
Jordan Newberry <jordan.newberry@gmail.com>
2021-06-10 15:22:44 -0500

    setting up tasks for the delete function

--- src/components/task/task.js
+++ src/components/task/task.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 
 const task = (props) => {
     const task = props.task;
+    const dispatch = useDispatch();
 
     return (
         <div>
             <h4>{task.task}</h4>
+            <button onClick={() => dispatch({ type: 'DELETE', payload: task.id })}>Delete</button>
         </div>
     );
 }
